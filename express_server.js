963bba4e01bfa3e1e15a0324c766c5e5e148c007
kmomozin <khinmomozin@gmail.com>
2023-01-23 18:24:09 -0700

    removing test code from server file

--- express_server.js
+++ express_server.js
@@ -10,18 +10,6 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
