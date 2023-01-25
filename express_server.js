c847f1d89dbfb31cbf38ceaec9362e2648d5f970
kmomozin <khinmomozin@gmail.com>
2023-01-24 18:20:34 -0700

    added cookie for username

--- express_server.js
+++ express_server.js
@@ -59,6 +59,12 @@ app.post("/urls/:id/update", (req, res) => {
   return res.redirect("/urls");
 });
 
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie('username', username);
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
